bb2e46d5928f7e6becb108d084b4296c62b920d9
habtew <habtamuwolde92@gmail.com>
2024-02-12 16:19:04 +0530

    codeforce simplfied

---
+++ components/problems.jsx
@@ -0,0 +1,72 @@
+export default function Problems(){
+    return (
+        <div className="problems--main">
+            <div className="filter-problems">
+                <ul>
+                    <li>
+                        array 322
+                    </li>
+                    <li>
+                        string 32
+                    </li>
+                    <li>
+                        greedy 123
+                    </li>
+                    <li>
+                        binarytree 543
+                    </li>
+                </ul>
+            </div>
+            
+            <div className="problems--table--main">
+                <div className="problems--table--header">
+                    <input type="text" placeholder="search" />
+                </div>
+            </div>
+            
+            <div className="problems--table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>status</th>
+                            <th>title</th>
+                            <th>acceptance</th>
+                            <th>difficulty</th>
+                            <th>total solved</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>+</td>
+                            <td>sort array in ascending order</td>
+                            <td>78%</td>
+                            <td>easy</td>
+                            <td>435453</td>
+                        </tr>
+                        <tr>
+                            <td>+</td>
+                            <td>sort array in ascending order</td>
+                            <td>78%</td>
+                            <td>easy</td>
+                            <td>435453</td>
+                        </tr>
+                        <tr>
+                            <td>+</td>
+                            <td>sort array in ascending order</td>
+                            <td>78%</td>
+                            <td>easy</td>
+                            <td>435453</td>
+                        </tr>
+                        <tr>
+                            <td>+</td>
+                            <td>sort array in ascending order</td>
+                            <td>78%</td>
+                            <td>easy</td>
+                            <td>435453</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
---
+++ index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import Problems from '/components/problems'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header >
+        <div className="nav--main">
+          <div className="user--profile">
+            <Link className="site-logo user--profile" to="/">
+              <img src="/images/favicon-32x32.png" className="user--profile--image" />
+              <p className="user--profile--name">habtew</p>
+            </Link>
+          </div>
+          <nav>
+            <Link to="/problems">Problems</Link>
+          </nav>
+        </div>
+      </header>
+      <Routes>
+        <Route path="/problems" element={<Problems />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+          // <Route path="contact" element={<ContactPage />} />
+          // <Route index element={<HomePage />} />
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
